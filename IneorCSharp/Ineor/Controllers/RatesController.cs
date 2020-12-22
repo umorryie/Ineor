be520267c0b4a0bd1dbb971a51dd37fdad97453b
Matej <pesjak.matej@gmail.com>
2020-12-22 11:51:19 +0100

    Added swagger

---
+++ IneorCSharp/Ineor/Controllers/RatesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ineor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RatesController : ControllerBase
+    {
+        private const string url = "https://euvatrates.com/rates.json";
+
+        public RatesController()
+        {
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            return "JAO";
+        }
+    }
+}
--- IneorCSharp/Ineor/Startup.cs
+++ IneorCSharp/Ineor/Startup.cs
@@ -26,6 +26,7 @@ namespace Ineor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,13 @@ namespace Ineor
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "swagger";
+            });
 
             app.UseHttpsRedirection();
 
