38dda7e3fdca7231da48fa5875136a739488dcf7
Matej <pesjak.matej@gmail.com>
2020-12-22 18:36:14 +0100

    Added 3 endpoints for rest API and added function to sort countries in order

--- IneorCSharp/Ineor/Controllers/RatesController.cs
+++ IneorCSharp/Ineor/Controllers/RatesController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ineor.Controllers
@@ -11,16 +14,72 @@ namespace Ineor.Controllers
     [Route("[controller]")]
     public class RatesController : ControllerBase
     {
-        private const string url = "https://euvatrates.com/rates.json";
+        private string url { get; set; }
 
         public RatesController()
         {
+            url = "https://euvatrates.com/rates.json";
+        }
+
+        [HttpPost]
+        [Route("countryVat/{countryCode}")]
+        public IActionResult  GetSpecificCountryInfo(string countryCode)
+        {
+            var urlString = new WebClient().DownloadString(url);
+            var jsonFromUrl = JObject.Parse(urlString);
+            var jsonRates = jsonFromUrl["rates"][countryCode];
+
+            if (jsonRates == null)
+            {
+                return NotFound( new { message = "Wrong country code. Try again." });
+            }
+
+            return Ok( jsonRates.ToObject<Country>() );
         }
 
         [HttpGet]
-        public String Get()
+        [Route("lowestVat")]
+        public IActionResult GetThreeLowestVatRates()
         {
-            return "JAO";
+            var urlString = new WebClient().DownloadString(url);
+            var jsonFromUrl = JObject.Parse(urlString);
+            var jsonRates = jsonFromUrl["rates"];
+
+            return Ok(GetVatArray(jsonRates, "lowest"));
+        }
+
+        [HttpGet]
+        [Route("highestVat")]
+        public IActionResult GetThreeHighestVatRates()
+        {
+            var urlString = new WebClient().DownloadString(url);
+            var jsonFromUrl = JObject.Parse(urlString);
+            var jsonRates = jsonFromUrl["rates"];
+
+            return Ok(GetVatArray(jsonRates, "highest"));
+        }
+
+        private List<Country> GetVatArray(JToken rates, string ratesValue)
+        {
+            var countryList = new List<Country> { };
+            IList<string> keys = JObject.Parse(rates.ToString()).Properties().Select(p => p.Name).ToList();
+
+            foreach (var key in keys)
+            {
+                countryList.Add(rates[key].ToObject<Country>());
+            }
+
+            // order list from lowest to highest VAT
+            countryList = countryList.OrderBy(o => o.standard_rate).ToList(); ;
+
+            if (ratesValue == "lowest")
+            {
+                return countryList.GetRange(0, 3);
+            }
+            else
+            {
+                return countryList.GetRange(countryList.Count - 3, 3);
+            }
         }
     }
 }
---
+++ IneorCSharp/Ineor/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ineor
+{
+    public class Country
+    {
+        public object country { get; set; }
+        public object standard_rate { get; set; }
+        public object reduced_rate { get; set; }
+        public object reduced_rate_alt { get; set; }
+        public object super_reduced_rate { get; set; }
+        public object parking_rate { get; set; }
+    }
+}
